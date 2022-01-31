fe6347f6c1ff223fddede7cff5bda30f3caf2443
Maarten Flippo <maartenflippo@outlook.com>
2022-01-31 12:00:29 +0100

    Move to a template, which has tests/lib interfaces better separated.

--- include/constraints/set.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../constraint.hpp"
-
-class SetInReif : public Constraint {
- public:
-  SetInReif(ConstraintBox constraintBox) : Constraint(constraintBox) {}
-  void loadVariables(const VariableMap& variableMap);
-  void configureVariables();
-};
---
+++ test/source/dummyTest.cpp
@@ -0,0 +1,3 @@
+#include <gtest/gtest.h>
+
+TEST(DummyTests, some_test) { EXPECT_EQ(1 + 1, 2); }
